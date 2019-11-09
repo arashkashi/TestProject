07b4349f01abc01cc809f8d30b1c5b51651e8c5a
Arash <arashkashi@gmail.com>
2019-11-09 06:13:56 +0100

    Initial Commit

---
+++ testWords/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testWords
+//
+//  Created by Arash Kashi on 2019-11-09.
+//  Copyright © 2019 Arash Kashi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
